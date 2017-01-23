a2c33981480d3d8fd7cf7e9adef36ccac6b4f028
Nathan Musoke <nathan.musoke@gmail.com>
2017-01-23 23:43:10 +1300

    Add layout for rest of aux2bib's structure

--- src/bin/aux2bib.rs
+++ src/bin/aux2bib.rs
@@ -8,6 +8,7 @@ use std::io::{Read, BufReader};
 
 fn main() {
 
+    // Define CLI
     let matches = App::new("aux2bib")
                         .version(crate_version!())
                         .author(crate_authors!(",\n"))
@@ -39,6 +40,20 @@ fn main() {
 
             let mut reader = BufReader::new(stdin);
             reader.read_to_string(&mut input_data).unwrap();
+        }
+    }
+
+    // Extract BibTeX tags from document
+
+    // Retrieve BibTeX entries from inspire.net
+
+    // Write BibTeX entries to file or stdout
+    match matches.value_of("OUTPUT") {
+        Some(output_file_name) => {
+            println!("Writing to file: {}", output_file_name);
+        },
+        None => {
+            println!("Writing to stdout");
         }
     }
 }
