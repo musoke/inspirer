73ee821fbf979389850e9febef9056a7936ce0d7
Nathan Musoke <nathan.musoke@gmail.com>
2017-06-25 13:17:28 +1200

    test: empty stdin input to binaries
    These should just return nothing.

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::process::Command;
+use std::process::{Command, Stdio};
 use std::path::PathBuf;
+use std::io::Write;
 
 fn get_bin_dir() -> PathBuf {
     env::current_exe()
@@ -24,7 +25,9 @@ fn cmd_aux2bib() -> Command {
     }
     println!("{:?}", path);
     let mut cmd = Command::new(path);
-    cmd.env_clear();
+    cmd.env_clear().stderr(Stdio::piped()).stdout(
+        Stdio::piped(),
+    );
 
     cmd
 }
@@ -40,7 +43,9 @@ fn cmd_blg2bib() -> Command {
         panic!("blg2bib binary {:?} was not found", path);
     }
     let mut cmd = Command::new(path);
-    cmd.env_clear();
+    cmd.env_clear().stderr(Stdio::piped()).stdout(
+        Stdio::piped(),
+    );
 
     cmd
 }
@@ -55,7 +60,6 @@ fn aux2bib_runs() {
     let error_code = cmd.wait().expect("Failed to wait on aux2bib");
 
     assert!(error_code.success());
-
 }
 
 #[cfg(not(windows))]
@@ -68,5 +72,40 @@ fn blg2bib_runs() {
     let error_code = cmd.wait().expect("Failed to wait on blg2bib");
 
     assert!(error_code.success());
+}
 
+#[cfg(not(windows))]
+#[test]
+fn aux2bib_stdin_empty() {
+    let mut child = cmd_aux2bib().stdin(Stdio::piped()).spawn().expect(
+        "Failed to execute aux2bib",
+    );
+
+    {
+        let stdin = child.stdin.as_mut().expect("Failed to get stdin");
+        stdin.write_all(b"").expect("Failed to write to stdin");
+    }
+
+    let output = child.wait_with_output().expect("Failed to wait on aux2bib");
+
+    assert!(output.status.success());
+    assert_eq!(output.stdout, []);
+}
+
+#[cfg(not(windows))]
+#[test]
+fn blg2bib_stdin_empty() {
+    let mut child = cmd_blg2bib().stdin(Stdio::piped()).spawn().expect(
+        "Failed to execute blg2bib",
+    );
+
+    {
+        let stdin = child.stdin.as_mut().expect("Failed to get stdin");
+        stdin.write_all(b"").expect("Failed to write to stdin");
+    }
+
+    let output = child.wait_with_output().expect("Failed to wait on aux2bib");
+
+    assert!(output.status.success());
+    assert_eq!(output.stdout, []);
 }
