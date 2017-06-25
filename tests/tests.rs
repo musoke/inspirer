fd5818cb84ab6d01da81a86b8db61cdcba0adaaf
Nathan Musoke <nathan.musoke@gmail.com>
2017-06-25 14:03:04 +1200

    test: Add test of aux2bib with actual input

--- tests/tests.rs
+++ tests/tests.rs
@@ -109,3 +109,59 @@ fn blg2bib_stdin_empty() {
     assert!(output.status.success());
     assert_eq!(output.stdout, []);
 }
+
+#[cfg(not(windows))]
+#[test]
+fn aux2bib_stdin_bibtex() {
+    let mut child = cmd_aux2bib().stdin(Stdio::piped()).spawn().expect(
+        "Failed to execute aux2bib",
+    );
+
+    {
+        let stdin = child.stdin.as_mut().expect("Failed to get stdin");
+        stdin
+            .write_all(
+                b"\
+\\relax 
+\\citation{Higgs:2014aqa}
+\\citation{Higgs:2015mei}
+\\bibstyle{unsrt}
+\\bibdata{test_bibtex}
+        ",
+            )
+            .expect("Failed to write to stdin");
+    }
+
+    let output = child.wait_with_output().expect("Failed to wait on aux2bib");
+
+    assert!(output.status.success());
+    // Output could conceivably change in future, so just check that some things are there
+    assert!(
+        std::str::from_utf8(&output.stdout).unwrap().contains(
+            "@article{Higgs:2014aqa,",
+        ) &&
+            std::str::from_utf8(&output.stdout).unwrap().contains(
+                "Higgs, Peter W.",
+            ) &&
+            std::str::from_utf8(&output.stdout).unwrap().contains(
+                "Nobel Lecture: Evading the Goldstone theorem",
+            ) &&
+            std::str::from_utf8(&output.stdout).unwrap().contains(
+                "2014",
+            )
+    );
+    assert!(
+        std::str::from_utf8(&output.stdout).unwrap().contains(
+            "@article{Higgs:2015mei,",
+        ) &&
+            std::str::from_utf8(&output.stdout).unwrap().contains(
+                "Higgs, P. W.",
+            ) &&
+            std::str::from_utf8(&output.stdout).unwrap().contains(
+                "Evading the Goldstone theorem",
+            ) &&
+            std::str::from_utf8(&output.stdout).unwrap().contains(
+                "2015",
+            )
+    );
+}
